28c011df8232e292575da66275d907b1957b8325
Alan Sempruch <alan.sempruch.rhs@gmail.com>
2021-03-15 15:59:24 -0400

    Updated util and vanilla LDA

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pickle
 
 # Loads data
 # returns: Tuple(all_data, X, y)
@@ -8,3 +9,28 @@ def load_data(path='../Files/', file='processed_data.csv'):
     y = proc_df[['perm_id','ticker_y','year_y','company_name','is_dividend_payer','dps_change','is_dps_cut','z_environmental','d_environmental','sector','filing_year_y']]
     
     return proc_df, X, y
+
+
+def unpickle(file, path='../Files/gensim/'):
+    with open(path+file, 'rb') as file:
+        obj = pickle.load(file)
+    return obj
+
+
+def load_gensim_data(year, path='../Files/gensim'):
+    if type(year) is list:
+        year_str = str(year[0]) + '-' + str(year[1])
+    else:
+        year_str = str(year)
+    
+    corpus = {
+        '1a': unpickle(f'{year_str}_item1a_corpus.pkl', path),
+        '7': unpickle(f'{year_str}_item7_corpus.pkl', path)
+    }
+
+    id2word = {
+        '1a': unpickle(f'{year_str}_item1a_id2word.pkl', path),
+        '7': unpickle(f'{year_str}_item7_id2word.pkl', path)
+    }
+    
+    return corpus, id2word
