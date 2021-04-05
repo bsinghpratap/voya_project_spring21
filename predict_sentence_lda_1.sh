d7b77552773d93fb2665e633bc99d91a411aa415
Harshul Shukla <hshukla@gypsum.cm.cluster>
2021-04-05 14:07:55 -0400

    Working viz

---
+++ predict_sentence_lda_1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#SBATCH --job-name=VOYA_5_predict
+#SBATCH --output=/mnt/nfs/scratch1/hshukla/prediction_results/output_%j.txt
+#SBATCH -e /mnt/nfs/scratch1/hshukla/prediction_results/error_%j.txt
+#SBATCH --partition=2080ti-long
+#SBATCH --gres=gpu:1
+#SBATCH --mem=150GB
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks-per-node=1
+
+JOB_TYPE='sentence_lda'
+LDA_RISK='/mnt/nfs/scratch1/hshukla/sentence_results/window1_rerunsen_lda_item1a_risk_1.model'
+LDA_MDA='/mnt/nfs/scratch1/hshukla/sentence_results/window1_rerunsen_lda_item7_mda_1.model'
+SCRIPT_DIR='/mnt/nfs/work1/696ds-s21/hshukla/voya_project_spring21/'
+INPUT='/mnt/nfs/scratch1/hshukla/sentence_results/df_sen_1_1.pkl'
+OUTPUT='/mnt/nfs/scratch1/hshukla/prediction_results/'
+WS=1
+START=2012
+END=2015
+PREDICT=2016
+cd $SCRIPT_DIR
+python3 Predict.py --job_type $JOB_TYPE --input_file $INPUT --output_folder $OUTPUT --lda_risk $LDA_RISK --lda_mda $LDA_MDA --window_size $WS --start_year $START --end_year $END --predict_year $PREDICT --pickled --corpus_filter
