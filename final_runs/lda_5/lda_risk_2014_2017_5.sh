8b9c558d35db64223663dd37b01231f2e91f6cfa
harshulshuk <hshukla@umass.edu>
2021-04-17 12:14:03 -0400

    added final run .sh files

---
+++ final_runs/lda_5/lda_risk_2014_2017_5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#SBATCH --job-name=VOYA_harshul_ws1
+#SBATCH --output=/mnt/nfs/scratch1/hshukla/sentence_results/window1_rerun/output_%j.txt
+#SBATCH -e /mnt/nfs/scratch1/hshukla/sentence_results/window1_rerun/error_%j.txt
+#SBATCH --partition=1080ti-long
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --mem=350GB
+#SBATCH --ntasks-per-node=8
+
+JOB_TYPE='sentence_lda'
+SCRIPT_DIR='/mnt/nfs/work1/696ds-s21/hshukla/voya_project_spring21/'
+INPUT='/mnt/nfs/scratch1/hshukla/final_results/data_sen_5_2.pkl'
+OUTPUT='/mnt/nfs/scratch1/hshukla/final_results/'
+WS=5
+START=2014
+END=2017
+LABEL="item1a_risk"
+cd $SCRIPT_DIR
+python3 Preprocessing.py --job_type $JOB_TYPE --input $INPUT --output_file $OUTPUT --window_size $WS --start_year $START --end_year $END --label $LABEL --pickled
