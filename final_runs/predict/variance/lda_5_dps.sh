d6d01cab478dfb9cfe9a3beb7da232b6a002c014
Harshul Shukla <hshukla@gypsum.cm.cluster>
2021-04-29 13:29:57 -0400

    changed variance .sh output location

--- final_runs/predict/variance/lda_5_dps.sh
+++ final_runs/predict/variance/lda_5_dps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 #SBATCH --job-name=VOYA_harshul_ws1
-#SBATCH --output=/mnt/nfs/scratch1/hshukla/final_predictions/lda_5_dps_%j.txt
-#SBATCH -e /mnt/nfs/scratch1/hshukla/final_predictions/error_%j.txt
+#SBATCH --output=/mnt/nfs/scratch1/hshukla/variance_predictions/lda_5_dps_%j.txt
+#SBATCH -e /mnt/nfs/scratch1/hshukla/variance_predictions/error_%j.txt
 #SBATCH --partition=1080ti-long
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
@@ -14,6 +14,6 @@ TARGET='is_dps_cut'
 WINDOW_SIZE=5
 SCRIPT_DIR='/mnt/nfs/work1/696ds-s21/hshukla/voya_project_spring21/'
 INPUT='/mnt/nfs/scratch1/hshukla/final_results/'
-OUTPUT='/mnt/nfs/scratch1/hshukla/final_predictions/'
+OUTPUT='/mnt/nfs/scratch1/hshukla/variance_predictions/'
 cd $SCRIPT_DIR
 python3 Predict.py --job_type $JOB_TYPE --target $TARGET --input_folder $INPUT --output_folder $OUTPUT --window_size $WINDOW_SIZE --variance
--- final_runs/predict/variance/tfidf_dps.sh
+++ final_runs/predict/variance/tfidf_dps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 #SBATCH --job-name=VOYA_harshul_ws1
-#SBATCH --output=/mnt/nfs/scratch1/hshukla/final_predictions/tfidf_dps_%j.txt
-#SBATCH -e /mnt/nfs/scratch1/hshukla/final_predictions/error_%j.txt
+#SBATCH --output=/mnt/nfs/scratch1/hshukla/variance_predictions/tfidf_dps_%j.txt
+#SBATCH -e /mnt/nfs/scratch1/hshukla/variance_predictions/error_%j.txt
 #SBATCH --partition=1080ti-long
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
@@ -13,6 +13,6 @@ JOB_TYPE='tfidf'
 TARGET='is_dps_cut'
 SCRIPT_DIR='/mnt/nfs/work1/696ds-s21/hshukla/voya_project_spring21/'
 INPUT='/mnt/nfs/scratch1/hshukla/final_results/'
-OUTPUT='/mnt/nfs/scratch1/hshukla/final_predictions/'
+OUTPUT='/mnt/nfs/scratch1/hshukla/variance_predictions/'
 cd $SCRIPT_DIR
 python3 Predict.py --job_type $JOB_TYPE --target $TARGET --input_folder $INPUT --output_folder $OUTPUT --variance
