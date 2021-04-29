aca9efdb3192231712c72e8639212ce473006153
harshulshuk <hshukla@umass.edu>
2021-04-29 10:57:36 -0400

    lda 1 feature creation

---
+++ final_runs/lda_1/create_features_2012_2015_5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=VOYA_harshul_ws1
+#SBATCH --output=/mnt/nfs/scratch1/hshukla/final_results/output_%j.txt
+#SBATCH -e /mnt/nfs/scratch1/hshukla/final_results/error_%j.txt
+#SBATCH --partition=1080ti-long
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --mem=150GB
+#SBATCH --ntasks-per-node=8
+
+JOB_TYPE='sentence_lda_features'
+SCRIPT_DIR='/mnt/nfs/work1/696ds-s21/hshukla/voya_project_spring21/'
+INPUT='/mnt/nfs/scratch1/hshukla/final_results/data_sen_1_1.pkl'
+OUTPUT='/mnt/nfs/scratch1/hshukla/final_results/'
+WS=1
+START=2012
+END=2015
+cd $SCRIPT_DIR
+python3 Processing.py --job_type $JOB_TYPE --input $INPUT --output_file $OUTPUT --window_size $WS --start_year $START --end_year $END --pickled
