42d7425d11bd2191a2b37ecdb07c9e0896a7622e
harshulshuk <hshukla@umass.edu>
2021-04-29 12:08:55 -0400

    Added variance + .sh files to run

---
+++ final_runs/predict/variance/tfidf_dps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=VOYA_harshul_ws1
+#SBATCH --output=/mnt/nfs/scratch1/hshukla/final_predictions/tfidf_dps_%j.txt
+#SBATCH -e /mnt/nfs/scratch1/hshukla/final_predictions/error_%j.txt
+#SBATCH --partition=1080ti-long
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --mem=150GB
+#SBATCH --ntasks-per-node=8
+
+JOB_TYPE='tfidf'
+TARGET='is_dps_cut'
+SCRIPT_DIR='/mnt/nfs/work1/696ds-s21/hshukla/voya_project_spring21/'
+INPUT='/mnt/nfs/scratch1/hshukla/final_results/'
+OUTPUT='/mnt/nfs/scratch1/hshukla/final_predictions/'
+cd $SCRIPT_DIR
+python3 Predict.py --job_type $JOB_TYPE --target $TARGET --input_folder $INPUT --output_folder $OUTPUT --variance
